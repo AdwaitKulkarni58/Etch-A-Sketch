1dd325bb3aed8b66761cf2651acf43e7ff8cf197
Adwait <adwait.kul.2018@gmail.com>
2023-01-11 11:07:22 -0800

    complete app

--- main.py
+++ main.py
@@ -4,15 +4,26 @@ my_turtle = Turtle()
 
 def move_forwards():
     my_turtle.forward(5)
-    
+
+
 def move_backwards():
-    my_turtle.left()
-    my_turtle.left()
     my_turtle.backward(5)
 
 
+def rotate_left():
+    my_turtle.left(5)
+
+
+def rotate_right():
+    my_turtle.right(5)
+
+
 my_screen = Screen()
-my_screen.onkeypress(move_forwards, "Right")
-my_screen.onkeypress(move_backwards, "Left")
+
+my_screen.onkeypress(move_forwards, "w")
+my_screen.onkeypress(move_backwards, "s")
+my_screen.onkeypress(rotate_left, "a")
+my_screen.onkeypress(rotate_right, "d")
+
 my_screen.listen()
 my_screen.exitonclick()
